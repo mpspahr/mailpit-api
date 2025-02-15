42fcac4994a17e0857dad730dd3496435e76c3f5
Matthew Spahr <mpspahr@gmail.com>
2025-02-15 04:26:27 -0600

    Fixed attachements and types
    * Fixed getMessageAttachment and getAttachmentThumbnail methods.
    ** Both methods now return: `{ data: ArrayBuffer, contentType: string }`
    * Separated EmailAddress into Email and Address interface
    * Updated lint packages
    * Fixed various type problems
    * Grouped the optional constructor auth parameters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+);
